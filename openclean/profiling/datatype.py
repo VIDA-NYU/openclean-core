ef24cd4027fbfc128b077effa35aa633b202aed2
Heiko Mueller <heiko.muller@gmail.com>
2020-10-20 09:29:41 -0400

    Change behavior for type cast to consider raw type of input (#45)

--- openclean/profiling/datatype.py
+++ openclean/profiling/datatype.py
@@ -10,42 +10,60 @@ value together with the type label for datatype counting and (min,max)
 computations.
 """
 
-from typing import Callable, List, Tuple
+from datetime import datetime
+from typing import Callable, List, Tuple, Type
 
 from openclean.data.types import Scalar
-from openclean.function.value.datatype import to_datetime, to_float, to_int
+from openclean.function.value.datatype import (
+    to_datetime, to_float, to_int, to_string
+)
 
 
 class DatatypeConverter(object):
     """Converter for scalar values that is used for profiling purposes. The
     converter maintains a list of datatype converters (callables) each of which
-    is assigned to a type label. Converters and their associated label are used
-    to represent data types. Converters are expected to return None for values
-    that do not belong to the respective data type.
+    is assigned to a raw type and a type label.
+
+    Converters and their associated label are used to represent the associated
+    raw data types. That is, converters are expected to return None for values
+    that cannot be converted to the respective raw data type.
 
     The datatype converter returns the converted value and type label for the
-    first converter that did not return None when called with a given value.
-    If no converter returned None the original value and a default label is
+    first converter that represents a raw data type to wich the given value
+    could be converted, i.e., that accepted the given value. If a value has a
+    raw type that matches the raw type of one of the converters the value
+    itself and the label for that respective converter is returned (Issue #45).
+
+    Note that the raw type for a given converter can be None. In this case
+    we ignore this converter when checking whether the raw type of an input
+    value matches the represented type of the converter, The converter may
+    still be used in case the raw type of an input value does not match the
+    raw types of any of the other converters and we attempt to cast the value.
+
+    If no converter accepted the original value and a default type label is
     returned.
     """
     def __init__(
-        self, datatypes: List[Tuple[str, Callable]], default_label: str
+        self, datatypes: List[Tuple[Type, str, Callable]], default_label: str
     ):
-        """Initialize the list of value converters and their associated type
-        labels.
+        """Initialize the list of value converters and their associated raw
+        type and type labels.
 
         Parameters
         ----------
         datatypes: list of tuples
-            Datatypes are represented by a tuple of a callable function and a
-            type label. the function is used to attemt to cast a given value to
-            the datatype that is represented by the converter.
+            Datatypes are represented by a tuple of raw data type, type label,
+            and a callable function that is used to attempt to cast a given
+            value to the datatype that is represented by the converter.
         default_label: string
             Default label for values that do not match any of the given data
             type converters.
         """
         self.datatypes = datatypes
         self.default_label = default_label
+        # Create a mapping of raw types to type labels for all converter that
+        # have a raw data type associated with them.
+        self.rawtypes = [(rt, lbl) for rt, lbl, _ in datatypes if rt]
 
     def convert(self, value: Scalar) -> Tuple[Scalar, str]:
         """Convert a given value. Returns a tuple of converted value and type
@@ -60,7 +78,14 @@ class DatatypeConverter(object):
         -------
         tuple of scalar and string (type label)
         """
-        for label, f in self.datatypes:
+        # Check if the given value has a type that matches any of the raw types
+        # of the associated converters.
+        for rawtype, label in self.rawtypes:
+            if isinstance(value, rawtype):
+                return value, label
+        # Attempt to convert the given value if it did not match the raw type
+        # of any of the converters.
+        for _, label, f in self.datatypes:
             val = f(value)
             if val is not None:
                 return val, label
@@ -78,9 +103,10 @@ def DefaultConverter() -> DatatypeConverter:
     """
     return DatatypeConverter(
         datatypes=[
-            ('int', to_int),
-            ('float', to_float),
-            ('date', to_datetime)
+            (int, 'int', to_int),
+            (float, 'float', to_float),
+            (datetime, 'date', to_datetime),
+            (None, 'str', to_string)
         ],
-        default_label='text'
+        default_label='unknown'
     )
---
+++ tests/profiling/test_convert_datatype.py
@@ -0,0 +1,33 @@
+# This file is part of the Data Cleaning Library (openclean).
+#
+# Copyright (C) 2018-2020 New York University.
+#
+# openclean is released under the Revised BSD License. See file LICENSE for
+# full license details.
+
+"""Unit tests for the datatype converter."""
+
+import pytest
+
+from datetime import datetime
+from openclean.profiling.datatype import DefaultConverter
+
+
+@pytest.mark.parametrize(
+    'input_value,result_label,result_value',
+    [
+        (1, 'int', 1),
+        ('2', 'int', 2),
+        (2.3, 'float', 2.3),
+        ('3.4', 'float', 3.4),
+        (datetime(2020, 5, 15), 'date', datetime(2020, 5, 15)),
+        ('2020-5-15', 'date', datetime(2020, 5, 15)),
+        ('ABC', 'str', 'ABC'),
+        ([1, 2, 3], 'unknown', [1, 2, 3])
+    ]
+)
+def test_convert_scalar_value(input_value, result_label, result_value):
+    """Test converting the data type of different input values."""
+    value, label = DefaultConverter().convert(input_value)
+    assert value == result_value
+    assert label == result_label
