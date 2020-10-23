c49477034d0461ee3c74dd46d656f13e0b4de5dd
Heiko Mueller <heiko.muller@gmail.com>
2020-10-23 09:09:19 -0400

    Make value picker a value function

--- openclean/function/value/picker.py
+++ openclean/function/value/picker.py
@@ -10,22 +10,74 @@
 
 from abc import ABCMeta, abstractmethod
 from collections import Counter
-from typing import Callable, Optional
+from typing import Callable, List, Optional, Union
 
 from openclean.data.types import Value
+from openclean.function.base import scalar_pass_through
+from openclean.function.value.base import CallableWrapper, ConstantValue, ValueFunction
 
 
 # -- Most frequent value picker interface -------------------------------------
 
-class MajorityCounter(metaclass=ABCMeta):
-    """Interface for majority pickers that select a single value from a set of
-    values for which their (absolute) frequency counts are given.
+class ValuePicker(ValueFunction, metaclass=ABCMeta):
+    """Value function that is used to select a single value, e.g., the most
+    frequent value, from a list of values. The picker acts as a value function
+    that picks a value when the function is prepared and then returns a constant
+    function that returns the picked value for any input value. This type of
+    behavior is for example needed for majority voting where we want to replace
+    all values in a given attribute with a single (most frequent) value.
     """
+    def __init__(self, raise_error: Optional[bool] = False):
+        """Initialize the behavior for the value function's prepare method if
+        no value in the given input list satisfies the constraints that are
+        defined by the value picker implementation. By default, the value picker
+        returns a scalar pass-trough function if no value is selected during
+        prepare, i.e., the pick method is called with the raise_error flag set
+        to False and a scalar_pass_through as the default value. The reason is
+        that if we want to use the returned function to update values in a
+        dataset attribute and there is no most frequent value then we do not
+        want to replace any values but keep the original ones. If the user wants
+        an error to be raised instead they can override the default behavior by
+        setting the raise_error flag when the class is instantiated.
+
+        Parameters
+        ----------
+        raise_error: bool, default=False
+            Raise a ValueError in the prepare method if the pick method does
+            not select a value for the given inputs.
+        """
+        self.raise_error = raise_error
+
+    def eval(self, value: Value) -> Value:
+        """The value picker requires to be prepared. It returns a new value
+        function  as the preparation result. The eval method of this class
+        therefore raises an error if called.
+
+        Parameters
+        ----------
+        value: scalar or tuple
+            Value from the list that was used to prepare the function.
+
+        Raises
+        -------
+        NotImplementedError
+        """
+        raise NotImplementedError()
+
+    def is_prepared(self) -> bool:
+        """The value picker needs to be prepared.
+
+        Returns
+        -------
+        bool
+        """
+        return False
+
     @abstractmethod
     def pick(
-        self, values: Counter, default: Optional[Value] = None,
+        self, values: Counter, default: Optional[Union[Callable, Value]] = None,
         raise_error: Optional[bool] = False
-    ) -> Value:
+    ) -> Union[Callable, Value]:
         """Picker function that returns the most frequent value from the given
         counter. Different implementations may impose additional constraints on
         whether a value is selected or not. If no value is selected by the
@@ -38,7 +90,7 @@ class MajorityCounter(metaclass=ABCMeta):
             Frequency counter for values. The most frequent value from the
             counter is returned if it satisfies additional (implementation-
             specific constraints).
-        default: scalar or tuple, default=None
+        default: scalar, tuple, or callable, default=None
             Default value that is returned if the most frequent value does not
             satisfy the imposed constraints and the raise_error flag is False.
         raise_error: bool, default=False
@@ -47,7 +99,7 @@ class MajorityCounter(metaclass=ABCMeta):
 
         Returns
         -------
-        scalar or tuple
+        scalar, tuple, or callabel
 
         Raises
         ------
@@ -55,10 +107,45 @@ class MajorityCounter(metaclass=ABCMeta):
         """
         raise NotImplementedError()
 
+    def prepare(self, values: List[Value]) -> ValueFunction:
+        """Optional step to prepare the function for a given set of values.
+        This step allows to compute additional statistics over the set of
+        values.
+
+        While it is likely that the given set of values represents the values
+        for which the eval() function will be called, this property is not
+        guaranteed.
+
+        Parameters
+        ----------
+        values: dict
+            Set of distinct scalar values or tuples of scalar values that are
+            mapped to their respective frequency count.
+
+        Returns
+        -------
+        openclean.function.value.base.ValueFunction
+        """
+        # Pick the most frequent value that satisfies the addition constraints
+        # of this picker. Use a scalar_pass_through as the default if no value
+        # is selected and the user did not set the raise_error flag when the
+        # object was constructed.
+        value = self.pick(
+            values=Counter(values),
+            default=scalar_pass_through,
+            raise_error=self.raise_error
+        )
+        if callable(value):
+            # The default value has been returned.
+            return CallableWrapper(func=value)
+        else:
+            # Return a constant value function for the picked value.
+            return ConstantValue(value=value)
+
 
 # -- Picker implementations ---------------------------------------------------
 
-class MostFrequentValue(MajorityCounter):
+class MajorityVote(ValuePicker):
     """Majority picker that select the most frequent value. This picker returns
     the default value (or raises an error) if the given list of values
     is empty or there are multiple-most frequent values.
@@ -68,7 +155,8 @@ class MostFrequentValue(MajorityCounter):
     """
     def __init__(
         self, threshold: Optional[float] = None,
-        normalizer: Optional[Callable] = None
+        normalizer: Optional[Callable] = None,
+        raise_error: Optional[bool] = False
     ):
         """Initialize the optional min. frequency thrshold and normalizer that
         will be applied to frequency values.
@@ -80,12 +168,17 @@ class MostFrequentValue(MajorityCounter):
             None.
         normalizer: callable, default=None
             Normalizer that is applied to the frequency value before selection.
+        raise_error: bool, default=False
+            Raise a ValueError in the picker method (instead of returning the
+            default pass-through function) if a given counter is empty or has
+            multiple most-frequent values.
         """
+        super(MajorityVote, self).__init__(raise_error=raise_error)
         self.threshold = threshold
         self.normalizer = normalizer
 
     def pick(
-        self, values: Counter, default: Optional[Value] = None,
+        self, values: Counter, default: Optional[Union[Callable, Value]] = None,
         raise_error: Optional[bool] = False
     ) -> Value:
         """Return the the most frequent value in the counter. If the counter is
@@ -96,7 +189,7 @@ class MostFrequentValue(MajorityCounter):
         ----------
         values: collections.Counter
             Frequency counter for values.
-        default: scalar or tuple, default=None
+        default: scalar, tuple, or callable, default=None
             Default value that is returned if the counter contains no values or
             multiple most-frequent values.
         raise_error: bool, default=False
@@ -105,7 +198,7 @@ class MostFrequentValue(MajorityCounter):
 
         Returns
         -------
-        scalar or tuple
+        scalar, tuple, or callabel
 
         Raises
         ------
@@ -139,12 +232,23 @@ class MostFrequentValue(MajorityCounter):
         return value
 
 
-class OnlyOneValue(MajorityCounter):
+class OnlyOneValue(ValuePicker):
     """Majority picker that only selects a value if the given counter contains
     exactly one value.
     """
+    def __init__(self, raise_error: Optional[bool] = False):
+        """Initialize the raise_error flag in the super class.
+
+        Parameters
+        ----------
+        raise_error: bool, default=False
+            Raise a ValueError in the picker method if a given counter is empty
+            or contains multiple values.
+        """
+        super(OnlyOneValue, self).__init__(raise_error=raise_error)
+
     def pick(
-        self, values: Counter, default: Optional[Value] = None,
+        self, values: Counter, default: Optional[Union[Callable, Value]] = None,
         raise_error: Optional[bool] = False
     ) -> Value:
         """Return the only value in the given counter. If the counter contains
@@ -156,7 +260,7 @@ class OnlyOneValue(MajorityCounter):
         values: collections.Counter
             Frequency counter for values. The most frequent value from the
             counter is returned if the counter contains exectly one value.
-        default: scalar or tuple, default=None
+        default: scalar, tuple, or callable, default=None
             Default value that is returned if the counter contains no values or
             multiple values.
         raise_error: bool, default=False
@@ -165,7 +269,7 @@ class OnlyOneValue(MajorityCounter):
 
         Returns
         -------
-        scalar or tuple
+        scalar, tuple, or callabel
 
         Raises
         ------
---
+++ tests/function/value/test_value_picker.py
@@ -0,0 +1,72 @@
+# This file is part of the Data Cleaning Library (openclean).
+#
+# Copyright (C) 2018-2020 New York University.
+#
+# openclean is released under the Revised BSD License. See file LICENSE for
+# full license details.
+
+"""Unit tests for different implementations of the majority picker class."""
+
+from collections import Counter
+
+import pytest
+
+from openclean.function.value.normalize import divide_by_total
+from openclean.function.value.picker import MajorityVote, OnlyOneValue
+
+@pytest.mark.parametrize(
+    'picker,values,result,raises_error',
+    [
+        (OnlyOneValue(), Counter({'A': 3}), 'A', False),
+        (OnlyOneValue(), Counter(), None, False),
+        (OnlyOneValue(), Counter(), None, True),
+        (OnlyOneValue(), Counter({'A': 3, 'B': 1}), 'Z', True),
+        (MajorityVote(), Counter({'A': 3}), 'A', False),
+        (MajorityVote(), Counter({'A': 3, 'B': 2}), 'A', False),
+        (MajorityVote(), Counter({'A': 3, 'B': 3}), None, False),
+        (MajorityVote(), Counter({'A': 3, 'B': 3}), 'C', True),
+        (MajorityVote(), Counter(), None, False),
+        (MajorityVote(), Counter(), None, True),
+        (MajorityVote(threshold=2), Counter({'A': 3}), 'A', False),
+        (MajorityVote(threshold=2), Counter({'A': 1}), 'B', True),
+        (MajorityVote(threshold=2), Counter({'A': 3, 'B': 3}), 'C', True),
+        (MajorityVote(threshold=0.7, normalizer=divide_by_total), Counter({'A': 3, 'B': 1}), 'A', False),
+        (MajorityVote(threshold=0.7, normalizer=divide_by_total), Counter({'A': 3, 'B': 2}), 'C', True)
+    ]
+)
+def test_most_frequent_picker(picker, values, result, raises_error):
+    """Test whether a given picker selects the expected value from a given
+    counter. If the raises_error flag is True the picker is called with the
+    raise_error flag True and is expected to raise a ValueError instead of
+    returning a value. Otherwise, the picker is only called with the raise_error
+    flag set to True if the expected result is not the default value.
+    """
+    if raises_error:
+        with pytest.raises(ValueError):
+            picker.pick(values, raise_error=True)
+    else:
+        assert picker.pick(values, raise_error=result is not None) == result
+
+
+@pytest.mark.parametrize(
+    'picker,values,input,result,raises_error',
+    [
+        (MajorityVote(), [1, 2, 2], 3, 2, False),
+        (MajorityVote(), [0, 1, 2], 3, 3, False),
+        (MajorityVote(raise_error=True), [0, 1, 2], 3, 3, True)
+    ]
+)
+def test_value_picker_function(picker, values, input, result, raises_error):
+    """Test the functionality of the value picker function for translating
+    values in a given input list.
+    """
+    if raises_error:
+        # If the raises_error flag is True then we expect the picker to raise a
+        # ValueError during prepare.
+        with pytest.raises(ValueError):
+            picker.prepare(values)
+    else:
+        # Execute the prepared picker on the given input and compare to the
+        # expected result.
+        f = picker.prepare(values)
+        assert f(input) == result
