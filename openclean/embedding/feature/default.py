e5ef77b5dc8203f08ad48282637a9771833311d2
Heiko Mueller <heiko.muller@gmail.com>
2021-01-05 13:52:08 -0500

    Update example notebooks

--- openclean/embedding/feature/default.py
+++ openclean/embedding/feature/default.py
@@ -41,3 +41,29 @@ class StandardEmbedding(FeatureEmbedding):
                 whitespace_fraction
             ]
         )
+
+
+class UniqueSetEmbedding(FeatureEmbedding):
+    """Instance of the feature embedding function for nique value stes. This
+    embedding ignores value frequencies. It uses a set of six value features to
+    compute feature vectors. The computed features are:
+    - normalized value length
+    - uniqueness of characters in the value string
+    - fraction of letter characters in the value string
+    - fraction of digits in the value string
+    - fraction of speical characters in the value string (not digit, letter, or
+      whitespace)
+    - fraction of whitespace characters in the value string
+    """
+    def __init__(self):
+        """Initialize the list of default value feature functions."""
+        super(UniqueSetEmbedding, self).__init__(
+            features=[
+                NormalizedLength(),
+                unique_fraction,
+                letters_fraction,
+                digits_fraction,
+                spec_char_fraction,
+                whitespace_fraction
+            ]
+        )
