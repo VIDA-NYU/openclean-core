98df3b7ef9b3dc050446691168017b12f630b885
Heiko Mueller <heiko.muller@gmail.com>
2021-01-27 12:29:40 -0500

    Change unit tests for old masterdata module

---
+++ tests/data/archive/test_data_archives.py
@@ -0,0 +1,53 @@
+# This file is part of the Data Cleaning Library (openclean).
+#
+# Copyright (C) 2018-2020 New York University.
+#
+# openclean is released under the Revised BSD License. See file LICENSE for
+# full license details.
+
+"""Unit tests for master data archive manager."""
+
+import os
+import pandas as pd
+import pytest
+
+import openclean.data.archive as masterdata
+import openclean.config as config
+
+
+DF_1 = pd.DataFrame(data=[[1, 2], [3, 4]], columns=['A', 'B'])
+DF_2 = pd.DataFrame(data=[[1, 2], [5, 6]], columns=['A', 'B'])
+
+
+def test_archive_manager(tmpdir):
+    """Test master data manager functionality."""
+    # -- Setup ----------------------------------------------------------------
+    os.environ[config.ENV_MASTERDATA_DIR] = str(tmpdir)
+    # Create two new archive for a given dataset.
+    masterdata.create('First', primary_key=['A'])
+    masterdata.create('Second', primary_key=['A'])
+    archive = masterdata.get('First')
+    archive.commit(DF_1)
+    archive = masterdata.get('First')
+    archive.commit(DF_2)
+    archive = masterdata.get('First')
+    assert len(archive.snapshots()) == 2
+    # -- Re-create archive ----------------------------------------------------
+    # Error when providing an existing name without replace.
+    with pytest.raises(ValueError):
+        masterdata.create('First', primary_key=['A'])
+    with pytest.raises(ValueError):
+        masterdata.create('Second', primary_key=['A'])
+    archive = masterdata.create('First', primary_key=['A'], replace=True)
+    archive.commit(DF_1)
+    # -- Delete archive -------------------------------------------------------
+    with pytest.raises(ValueError):
+        masterdata.delete('unknown')
+    masterdata.delete('First')
+    # Error when deleting or accessing an unknown archive.
+    with pytest.raises(ValueError):
+        masterdata.delete('First')
+    with pytest.raises(ValueError):
+        masterdata.get('First')
+    # -- Cleanup --------------------------------------------------------------
+    del os.environ[config.ENV_MASTERDATA_DIR]
