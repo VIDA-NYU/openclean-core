148ae2d3af2050c6baf11e2b6ac0e2745b8dcbc2
Heiko Mueller <heiko.muller@gmail.com>
2021-02-25 13:37:53 -0500

    Add helper function for getting flowServ worker factory from environment

--- openclean/config.py
+++ openclean/config.py
@@ -10,8 +10,12 @@ are maintained in environment variables.
 """
 
 from appdirs import user_cache_dir
+from flowserv.controller.serial.worker.factory import WorkerFactory, convert_config
+from typing import List, Optional
 
+import json
 import os
+import yaml
 
 
 """Environment variables that maintain configuration parameters."""
@@ -21,6 +25,8 @@ ENV_DATA_DIR = 'OPENCLEAN_DATA_DIR'
 ENV_MASTERDATA_DIR = 'OPENCLEAN_MASTERDATA_DIR'
 # Number of parallel threads to use.
 ENV_THREADS = 'OPENCLEAN_THREADS'
+# Configuration file containing the configuration for flowServ workers.
+ENV_WORKERS = 'OPENCLEAN_WORKERS'
 
 
 def DATADIR() -> str:
@@ -60,3 +66,61 @@ def THREADS() -> int:
     except ValueError:
         threads = 1
     return 1 if threads < 1 else threads
+
+
+def WORKERS(var: Optional[str] = None) -> WorkerFactory:
+    """Create a worker factory for serial workflow execution from a configuration
+    file.
+
+    By default, the configuration is read from the file that is referenced by
+    the environment variable `OPENCLEAN_WORKERS`. If the optional environment
+    variable `var` is given and references an existing file, the configurations
+    from that file will override the default configuration.
+
+    Parameters
+    ----------
+    var: string, default=None
+        Optional name for an environment variable that references a file with
+        flowServ worker configurations that will override the values in the
+        default configuration.
+
+    Returns
+    -------
+    flowserv.controller.serial.worker.factory.WorkerFactory
+    """
+    worker_conf = convert_config(read_list(os.environ.get(ENV_WORKERS)))
+    if var:
+        worker_conf.update(convert_config(read_list(os.environ.get(var))))
+    return WorkerFactory(config=worker_conf)
+
+
+# -- Helper Functions ---------------------------------------------------------
+
+def read_list(filename: str) -> List:
+    """Read a list object from a given file.
+
+    The file type is guessed from the file suffix. If the file ends with '.json'
+    it is assumed to be in Json format. Otherwise, Yaml format is assumed by
+    default.
+
+    If the filename is None or empty an empty list is returned.
+
+    Parameters
+    ----------
+    filename: string
+        Path to file on disk.
+
+    Returns
+    -------
+    list
+    """
+    # Return an empty list of the filename is not given
+    if not filename:
+        return list()
+    # Guess the file type from the name suffix. By default, Yaml is assumed.
+    if filename.endswith('.json'):
+        with open(filename, 'r') as f:
+            return json.load(f)
+    else:
+        with open(filename, 'r') as f:
+            return yaml.load(f.read(), Loader=yaml.FullLoader)
